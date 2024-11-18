ff177e22e975079238f7f74e2454287aa2bb6cb3
Long-Soeng168 <generalpurpose167@gmail.com>
2024-11-18 10:56:41 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,17 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $websiteInfo = WebsiteInfo::first() ?? new WebsiteInfo;
-        View::share('websiteInfo', $websiteInfo);
-
-        $menu_databases = Database::where('status', 1)->orderBy('order_index', 'ASC')->get() ?? new Database;
-        View::share('menu_databases', $menu_databases);
-
-        $menu_database_default = new Database;
-        $menu_database_default['name'] = 'All';
-        $menu_database_default['name_kh'] = 'ទាំងអស់';
-        $menu_database_default['slug'] = 'one_search';
-        View::share('menu_database_default', $menu_database_default);
 
         $footer = Footer::first() ?? new Footer;
         View::share('footer', $footer);
